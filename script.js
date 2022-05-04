39758c074526669a6cf4705e49100e7ff04dfc95
Jonny <jonny.hall5800@gmail.com>
2022-05-04 19:46:49 +0100

    Error property of null (setting "innerHTML"

---
+++ script.js
@@ -0,0 +1,31 @@
+const days = document.getElementById("days")
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+//to update the countdown
+function updateCountdownTime() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+    console.log(diff)
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerhtml = d
+
+
+    /*
+            days.innerHTML = d;
+            hours.innerHTML = h < 10 ? "0" + h : h;
+            minutes.innerHTML = m < 10 ? "0" + m : m;
+            seconds.innerHTML = s
+            console.log(d)*/
+}
+//setInterval(updateCountdownTime, 1000)
+setInterval(updateCountdownTime, 1000)
